a81f89d08115a2adb69b5325c6c2af532258ac74
Gleymar Peña <esgleymar@hotmail.com>
2026-08-24 22:18:43 -0400

    Se agrego state machine paused

--- 02-flappy_bird/src/FlappyBird.py
+++ 02-flappy_bird/src/FlappyBird.py
@@ -26,6 +26,7 @@ class FlappyBird(Game):
                 "title": states.TitleScreenState,
                 "count_down": states.CountDownState,
                 "playing": states.PlayingState,
+                "paused" : states.PauseState,
             }
         )
         self.state_machine.change("title")
