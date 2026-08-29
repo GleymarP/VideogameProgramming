4fe69ac3e2ed9f4b4e695a811b82aef3aaad76b3
Gleymar Peña <esgleymar@hotmail.com>
2026-08-28 22:55:02 -0400

    Se agregó temporizador para fantamas y el renderizado condicional para la apariencia del fantasma

--- 02-flappy_bird/src/Bird.py
+++ 02-flappy_bird/src/Bird.py
@@ -9,9 +9,7 @@ This file contains the definition of the class Bird.
 """
 
 import pygame
-
 import settings
-
 
 class Bird:
     def __init__(self, x: float, y: float, width: float, height: float) -> None:
@@ -23,11 +21,18 @@ class Bird:
         self.vx: float = 0.0
         self.jumping: bool = False
 
+        self.is_ghost = False
+        self.ghost_timer = 0.0
+      
     def get_rect(self) -> pygame.Rect:
         return pygame.Rect(round(self.x), round(self.y), self.width, self.height)
-
+    
     def jump(self) -> None:
         self.jumping = True
+
+    def active_ghost_mode (self, duration: float) -> None:
+        self.is_ghost = True
+        self.ghost_timer = duration
 
     def update(self, dt: float) -> None:
         self.vy += settings.GRAVITY * dt
@@ -42,5 +47,17 @@ class Bird:
         self.x += self.vx * dt
         self.x = max(0, min(self.x, settings.VIRTUAL_WIDTH - self.width))
 
+        if self.is_ghost:
+            self.ghost_timer -= dt
+            if self.ghost_timer <= 0:
+                self.is_ghost = False
+                self.ghost_timer = 0.0
+
     def render(self, surface: pygame.Surface) -> None:
-        surface.blit(settings.TEXTURES["bird"], self.get_rect())
+        if self.is_ghost:
+            surface.blit(settings.TEXTURES["ghost"], self.get_rect())   
+        else:
+            surface.blit(settings.TEXTURES["bird"], self.get_rect())   
+
+       
+             
