91f4ee0d7ee10ddea11e45eb7ee6a00de6d8d3df
Gleymar Peña <esgleymar@hotmail.com>
2026-08-29 00:08:08 -0400

    Se implementa patron strategy para definir comportamientos, generacion de troncos y poweup segun la dificultad

--- 02-flappy_bird/src/Strategy.py
+++ 02-flappy_bird/src/Strategy.py
@@ -1,15 +1,34 @@
 import random
 import settings
 from gale.input_handler import InputData
+from gale.factory import Factory
+from src.LogPair import LogPair
 
 class NormalStrategy:
     def handle_bird_movement(self, bird, input_id: str, input_data: InputData) -> None:
         if input_id == "jump" and input_data.pressed:
             bird.jump()
 
+    def get_spawn_interval(self) -> float:
+         return settings.TIME_TO_SPAWN_LOGS
+
+    def calculate_log_y(self, last_y: float) -> float:
+         return max(
+                    -settings.LOG_HEIGHT + 10,
+                        min(
+                            last_y + random.randint(-20, 20),
+                            settings.VIRTUAL_HEIGHT + 90 - settings.LOG_HEIGHT,
+                    ),
+                )
+
+    def create_log_pair(self, factory: Factory, x: float, y: float) -> LogPair:
+         return factory.create(x, y, properties={ "moving": False })
+
+    def spawn_powerup(self, factory:Factory, x: float, y: float):
+         return None
+
 class HardStrategy:
     def handle_bird_movement(self, bird, input_id: str, input_data: InputData) -> None:
-
         if input_id == "move_left":
             if input_data.pressed:
                 bird.vx = -settings.BIRD_SPEED
@@ -25,4 +44,23 @@ class HardStrategy:
         if input_id == "jump" and input_data.pressed:
                     bird.jump()
 
-    
+    def get_spawn_interval(self) -> float:
+         return random.uniform(1 , 30)
+
+    def calculate_log_y(self, last_y: float) -> float:
+         target_y = last_y + random.randint(-50, 50)
+
+         min_y = -settings.LOG_HEIGHT + 20
+         max_y = settings.VIRTUAL_HEIGHT - settings.GROUND_HEIGHT - settings.LOG_HEIGHT - 20
+
+         return max(min_y, min(target_y, max_y))
+
+    def create_log_pair(self, factory:Factory, x: float, y:float) -> LogPair:
+        is_moving = random.choice([True, False])
+
+        return factory.create(x, y, properties={"moving": is_moving})
+
+    def spawn_powerup(self, factory: Factory, x: float, y: float):
+         if random.random() < 0.5:
+              return factory.create(x, y)
+         return None
