22bbc67f1eee20a4db53eec0d70328f606a1e9ba
Gleymar Peña <esgleymar@hotmail.com>
2026-08-30 22:19:29 -0400

    Se agrego en el constructor variables para controlar si sticky esta activo, si la pelota esta adherida y en que posicion se pego

--- 03-breakout/src/Paddle.py
+++ 03-breakout/src/Paddle.py
@@ -32,6 +32,8 @@ class Paddle:
         # The paddle only move horizontally
         self.vx = 0
 
+        self.sticky = False
+
     def resize(self, size: int) -> None:
         self.size = size
         self.width = (self.size + 1) * 32
